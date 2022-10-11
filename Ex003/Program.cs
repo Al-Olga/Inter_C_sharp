03042a8dff154191a1a6bbfb2fa38bd900cae072
Olga_A <zagaes2@rambler.ru>
2022-10-11 11:01:29 +0300

    Created program Ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задать номер четверти, 
+// показать диапазоны для возможных координат
+
+int NumberQuarter = new Random().Next(1,5);
+Console.WriteLine(NumberQuarter);
+
+if (NumberQuarter == 1) 
+    Console.WriteLine("Для четверти I возможны значения: x>0 и y>0");
+else if (NumberQuarter == 2) 
+    Console.WriteLine("Для четверти II возможны значения: x<0 и y>0");
+else if (NumberQuarter == 3) 
+    Console.WriteLine("Для четверти III возможны значения: x<0 и y<0");
+else 
+    Console.WriteLine("Для четверти IV возможны значения: x>0 и y<0");
