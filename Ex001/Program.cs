90245aae51067f53bdbbff4bb27ac9833e2d5a58
Olga_A <zagaes2@rambler.ru>
2022-10-11 10:30:39 +0300

    Created program Ex001

---
+++ Ex001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число обозначающее день недели. 
+// Выяснить является номер дня недели выходным
+
+int NumberWeek;
+
+Console.Write("Введите номер дня недели  = ");
+NumberWeek = Convert.ToInt32(Console.ReadLine());
+
+if ((NumberWeek == 6) | (NumberWeek == 7)) 
+    Console.WriteLine("Введенное число соответствует Выходному дню");
+else Console.WriteLine("Введенное число не является Выходным днем");
