b61c2f4104a83576f924ddae55ede5937c0c71b6
Olga_A <zagaes2@rambler.ru>
2022-10-11 10:41:13 +0300

    Created program Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,16 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+int a;
+int b;
+
+Console.Write("Введите первое число А = ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число B = ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b*b) 
+    Console.WriteLine("Число А={0} является квадратом числа В={1}", a, b);
+else if (b == a*a) 
+    Console.WriteLine("Число В={0} является квадратом числа А={1}", b, a);
+else
+    Console.WriteLine("Числа А={0} и В={1} не являются квадратами друг-друга", a, b);
